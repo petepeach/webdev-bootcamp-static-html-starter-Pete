e703d6f3019d07a899ebb79bb23de153f8661649
Weraphat Nimchaiyanan <35192556+petepeach@users.noreply.github.com>
2023-12-23 14:27:38 +0700

    Create header website

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DEF81D",
+        primary: "#90D6E2",
         "wd-white": "#FFFFFF",
         "wd-grey": "#999999",
         "wd-darkgrey": "#2D2D2D",
